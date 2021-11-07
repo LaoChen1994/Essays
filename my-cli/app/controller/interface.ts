100fd79a76f0fe959c95f1ff005f3c8ca7bab78d
chenyixiang <chenyixiang_cyx@youzan.com>
2021-11-07 12:25:05 +0800

    feat: Essay 文档补充

--- my-cli/app/controller/interface.ts
+++
@@ -1,7 +0,0 @@
-import Koa from 'koa'
-
-export interface IControllerCtx extends Koa<Koa.DefaultState, Koa.DefaultContext>  {
-    render: (path: string) => void
-    setState: (state: Record<string, any>) => void
-    setHeader: (opts: Record<string, string>) => void
-}
